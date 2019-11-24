98a391a581842d15ed8040dcd2c9399d6039be73
UULIN <1036190402@qq.com>
2019-11-24 23:53:27 +0800

    20191124

--- nine/car.py
+++ nine/car.py
@@ -6,7 +6,7 @@ class Car:
         self.model = model
         self.year = year
         self.odometer_reading = 0
-
+        self.gas = 0
     def get_descriptive_name(self):
         lone_name = str(self.year) + " " + self.make + " " + self.model
         return lone_name.title()
@@ -14,3 +14,20 @@ class Car:
     def read_odometer(self):
         """打印汽车里程信息"""
         print("This car " + str(self.odometer_reading) + "'s miles on it.")
+
+    def update_odometer(self, mileage):
+        if mileage >= self.odometer_reading:
+
+            self.odometer_reading = mileage
+        else:
+            print("you can't roll back an odometer")
+
+    def increment_odometer(self, miles):
+        """将里程表读数增加指定的量"""
+        self.odometer_reading += miles
+
+    def fill_gas_tank(self, increment_gas):
+        """车油箱"""
+        self.gas += increment_gas
+        print("car's gas is " + str(self.gas))
+
---
+++ nine/new_electric_car.py
@@ -0,0 +1,9 @@
+from nine.car import *
+my__tesla = ElectricVar('tesla', 'model s', 2016)
+
+print(my__tesla.get_descriptive_name())
+
+
+my__tesla.battery.describe_battery()
+my__tesla.battery.get_range()
+my__tesla.fill_gas_tank()
