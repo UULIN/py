bd68e7bc9e7a9462ad95633dba00bd47bd5211c5
UULIN <1036190402@qq.com>
2019-12-03 23:42:35 +0800

    191202

---
+++ fifteen/scatter_squares.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+
+x_values = [1, 2, 3, 4, 5]
+y_values = [1, 4, 9, 16, 25]
+plt.scatter(2, 4, s=200)
+# 设置图标标题并给坐标轴加上标签
+plt.title("Square Numbers", fontsize=24)
+plt.xlabel("Value", fontsize=14)
+plt.ylabel("Square of Value", fontsize=14)
+
+# 设置刻度标记大小
+plt.tick_params(axis='both', which='major', labelsize=14)
+
+plt.scatter(x_values, y_values, s=100)
+plt.show()
