6f99e40d94b95a83640bcd3a3255a730b92175f8
UULIN <1036190402@qq.com>
2019-12-06 22:58:57 +0800

    191206

--- fifteen/die.py
+++ fifteen/die.py
@@ -1,3 +1,10 @@
 from random import randint
 class Die():
-    """"""
+    """表示一个骰子的类"""
+    def __init__(self, num_sides=6):
+        """骰子默认6面"""
+        self.num_sides = num_sides
+
+    def roll(self):
+        """返回一个1到骰子最大值的随机值"""
+        return randint(1, self.num_sides)
---
+++ fifteen/die_visual.py
@@ -0,0 +1,36 @@
+from fifteen.die import Die
+import pygal
+"""投骰子"""
+# 创建2个骰子
+die1 = Die()
+die2 = Die()
+
+# 投几次骰子，放入一个列表
+results = []
+for roll_num in range(1000):
+    result = die1.roll() + die2.roll()
+    results.append(result)
+
+# 分析结果
+frequencies = []
+max_result = die1.num_sides + die2.num_sides
+for value in range(2, max_result + 1):
+
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+# 进行结果可视化
+hist = pygal.Bar()
+hist.title = "Results of rolling two D6 1000 times"
+hist.x_labels = []
+
+for label_num in range(2, 13):
+    hist.x_labels.append(label_num)
+
+hist.x_title = "Result"
+hist.y_title = "Frequency of Result"
+
+hist.add('D6', frequencies)
+hist.render_to_file('./data/die_visual.svg')
+
+print(frequencies)
