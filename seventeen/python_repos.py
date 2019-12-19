8a3f7daecff12564ac582655c0895a37553c1c41
UULIN <1036190402@qq.com>
2019-12-19 23:11:13 +0800

    1211

--- seventeen/python_repos.py
+++ seventeen/python_repos.py
@@ -1,4 +1,6 @@
 import requests
+import pygal
+from pygal.style import LightColorizedStyle as lcs, LightenStyle as ls
 
 # 执行API调用并存储响应
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
@@ -12,10 +14,28 @@ print("Total repositories:", response_dict['total_count'])
 
 # 探索有关仓库的信息
 repo_dicts = response_dict['items']
-print('Repositories returned:', len(repo_dicts))
+# print('Repositories returned:', len(repo_dicts))
 
 # 研究第一个仓库
-repo_dict = repo_dicts[0]
-print("\nKeys:", len(repo_dict))
-for key in repo_dict.keys():
-    print(key)
+# repo_dict = repo_dicts[0]
+names, stars = [], []
+# print("\nSelected information about each repository:")
+for repo_dict in repo_dicts:
+    names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+
+# 可视化
+my_style = ls('#333366', base_style=lcs)
+chart = pygal.Bar(style=my_style, x_lable_rotation=45, show_legend=False)
+chart.title = "Most-Starred Pyhon Projects on Github"
+chart.x_labels = names
+chart.add('', stars)
+chart.render_to_file('./data/python_repos.svg')
+
+"""  print('name:', repo_dict['name'])
+    print('Onwer:', repo_dict['owner']['login'])
+    print('Stars:',repo_dict['stargazers_count'])
+    print('Repository:', repo_dict['html_url'])
+    print('Created:', repo_dict['created_at'])
+    print('Updated:', repo_dict['updated_at'])
+    print('Description:', repo_dict['description'])"""
