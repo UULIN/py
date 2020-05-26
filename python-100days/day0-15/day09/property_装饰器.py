9de19be33fd52b1f66479010502a517baf682d33
UULIN <1036190402@qq.com>
2020-05-26 23:13:16 +0800

    day08day09

---
+++ python-100days/day0-15/day09/property_装饰器.py
@@ -0,0 +1,44 @@
+class Person(object):
+
+    __solt__ = ('_name', '_age', '_gender')
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    # 访问器 getter方法
+    @property
+    def name(self):
+        return self._name
+
+    # 访问器 getter方法
+    @property
+    def age(self):
+        return self._age
+
+    # 修改器 setter方法
+    @age.setter
+    def age(self, age):
+        self._age = age
+
+    @name.setter
+    def name(self, name):
+        self._name = name
+
+    def play(self):
+        if self._age < 16:
+            print('%s正在玩飞行棋' % self._name)
+        else:
+            print('%s正在玩斗地主' % self._name)
+
+
+def main():
+    person = Person('王大锤', 18)
+    person.play()
+    person.age = 14
+    person.play()
+    person._gender = '男'
+    person._is_gay = True
+
+
+main()
